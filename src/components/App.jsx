a26ca404c53e85c755b8d3f84b6599f9ca606db5
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-06-21 12:57:10 +0200

    Inject all to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { Section } from './Section';
+import { FeedbackOptions } from './FeedbackOptions';
+import theme from './styled/theme';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = id => {
+    this.setState(prev => {
+      return {
+        [id]: prev[id] + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={[
+              {
+                name: 'Good',
+                id: 'good',
+              },
+              {
+                name: 'Neutral',
+                id: 'neutral',
+              },
+              {
+                name: 'Bad',
+                id: 'bad',
+              },
+            ]}
+            onLeaveFeedback={this.leaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics"></Section>
+      </ThemeProvider>
+    );
+  }
+}
